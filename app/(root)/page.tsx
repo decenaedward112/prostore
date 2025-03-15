18ba1e1400c65247f7c972d687ba6719350bbc5b
Edward Decena <edwarddecena@Edwards-MacBook-Air.local>
2025-03-15 16:22:18 +0800

    add products

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,14 @@
-const delay = (ms: number | undefined) => new Promise((resolve) => setTimeout(resolve, ms));
+import ProductList from '@/components/shared/product/product-list';
+import sampleData from '@/db/sample-data';
 
-const Homepage = async () => {
-    await delay(1000);
-  return <>Prostore</>;
+// const delay = (ms: number | undefined) => new Promise((resolve) => setTimeout(resolve, ms));
+
+const HomePage = () => {
+  return (
+    <div className='space-y-8'>
+      <h2 className='h2-bold'>Latest Products</h2>
+      <ProductList title='Newest Arrivals' data={sampleData.products} limit={4} />
+    </div>
+  );
 };
-
-export default Homepage;
+export default HomePage;
