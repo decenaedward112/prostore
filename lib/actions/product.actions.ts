92e4dd826fb8706d663ff3d84f5afca565a1fb7e
Edward Decena <edwarddecena@Edwards-MacBook-Air.local>
2025-03-29 17:16:37 +0800

    add single product page

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -4,6 +4,7 @@ import { convertToPlainObject } from "../utils"
 import { LATEST_PRODUCTS_LIMIT } from "../constants"
 import { prisma } from "@/db/prisma"
 
+//Get latest products
 export async function getLatestProducts() {
     const data = await prisma.product.findMany({
         take: LATEST_PRODUCTS_LIMIT,
@@ -11,4 +12,11 @@ export async function getLatestProducts() {
     })
     
     return convertToPlainObject(data)
+}
+
+// Get Product by slug
+export async function getProductBySlug(slug: string) {
+    return await prisma.product.findFirst({
+        where: { slug },
+    })
 }
